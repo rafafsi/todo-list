78513b804955738a469c8435625b2fb7fd47c908
Rafaela Inácio <rafaelafsinacio@gmail.com>
2023-05-19 20:47:58 -0300

    main structure

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser")
+const app = express();
+
+//middlewares
+app.use(cors()); //takes care of security of our application
+app.use(bodyParser.json())
+
+//db connection
+require("./db/config");
+
+//requiring routes
+app.use(require('./db/routes'))
+
+app.listen(3000, () => console.log("running on port 3000"));
+
+
