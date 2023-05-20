529f961f86e26e7dc0b1e80b9fe785bf6843544e
Rafaela Inácio <rafaelafsinacio@gmail.com>
2023-05-20 19:35:44 -0300

    try/catch blocks added and errors handled

--- db/controllers/tag/tag.js
+++ db/controllers/tag/tag.js
@@ -3,35 +3,49 @@ const Task = require("../../models/task");
 
 module.exports = {
   create: async (req, res) => {
-    const { name, color } = await req.body;
-
-    const tag = await Tag.create({ name, color }); //tag to be added
-    await tag.save();
-
-    let taskById = await Task.findByIdAndUpdate(req.params.id, {
-      $push: {
-        tags: tag._id,
-      },
-    });
-    res.send(taskById);
+    try {
+      const { name, color } = await req.body;
+      const tag = await Tag.create({ name, color }); 
+      await tag.save();
+      let taskById = await Task.findByIdAndUpdate(req.params.id, {
+        $push: {
+          tags: tag._id,
+        },
+      });
+      res.send(taskById);
+    } catch (error) {
+      res.send(error.message);
+    }
   },
   find: async (req, res) => {
-    const tagFound = await Tag.findById(req.params.id);
-    res.send(tagFound);
+    try {
+      const tagFound = await Tag.findById(req.params.id);
+      res.send(tagFound);
+    } catch (error) {
+      res.send(error.message);
+    }
   },
   update: async (req, res) => {
-    const data = req.body;
-    const tagFound = await Tag.findByIdAndUpdate(req.params.id, {
-      $set: {
-        name: data.name,
-        color: data.color,
-      },
-    });
-    await tagFound.save();
-    res.send(tagFound);
+    try {
+      const data = req.body;
+      const tagFound = await Tag.findByIdAndUpdate(req.params.id, {
+        $set: {
+          name: data.name,
+          color: data.color,
+        },
+      });
+      await tagFound.save();
+      res.send(tagFound);
+    } catch (error) {
+      res.send(error.message);
+    }
   },
   delete: async (req, res) => {
-    const deletedTag = await Tag.findByIdAndDelete(req.params.id);
-    await res.send(deletedTag);
+    try {
+      const deletedTag = await Tag.findByIdAndDelete(req.params.id);
+      await res.send(deletedTag);
+    } catch (error) {
+      res.send(error.message);
+    }
   },
 };
--- db/controllers/task/task.js
+++ db/controllers/task/task.js
@@ -1,37 +1,63 @@
+const { connection } = require("mongoose");
 const Task = require("../../models/task");
 
 module.exports = {
   create: async (req, res) => {
-    const { title, status, priority, description } = req.body;
-    const task = await Task.create({ title, status, priority, description });
-    res.send(task);
+    try {
+      const { title, status, priority, description } = req.body;
+      const task = await Task.create({ title, status, priority, description });
+      task.save();
+      res.send(task);
+    } catch (error) {
+      res.send(error.message);
+    }
   },
   find: async (req, res) => {
-    const taskFound = await Task.findById(req.params.id);
-    res.send(taskFound);
+    try {
+      const taskFound = await Task.findById(req.params.id);
+      res.send(taskFound);
+    } catch (error) {
+      res.send(error.message);
+    }
   },
   update: async (req, res) => {
-    const data = req.body;
-    let task = await Task.findByIdAndUpdate(req.params.id, {
-      $set: {
-        title: data.title,
-        status: data.status,
-        priority: data.priority,
-        description: data.description,
-      },
-    });
-    task.save();
-    res.send(task);
+    try {
+      const data = req.body;
+      let task = await Task.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            title: data.title,
+            status: data.status,
+            priority: data.priority,
+            description: data.description,
+          },
+        },
+        { returnDocument: "after", runValidators: true, new: true }
+      );
+      task.save();
+      res.send(task);
+    } catch (error) {
+      res.send(error.message);
+    }
   },
   delete: async (req, res) => {
-    const deletedTask = await Task.findByIdAndDelete(req.params.id);
-    res.send(deletedTask);
+    try {
+      const taskDeleted = await Task.findByIdAndDelete(req.params.id);
+      res.send(taskDeleted);
+    } catch (error) {
+      res.send(error.message);
+    }
   },
   populateTags: async (req, res) => {
-    const taskFound = await Task.findById(req.params.id).populate(
-      "tags",
-      "-_id -__v"
-    );
-    res.send(taskFound);
+    try {
+      const taskFound = await Task.findById(req.params.id).populate(
+        "tags",
+        "-_id -__v"
+      );
+      res.send(taskFound);
+    } catch (error) {
+      res.send(error.message);
+    }
   },
 };
