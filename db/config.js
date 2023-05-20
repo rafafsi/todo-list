c9245d443436583e18b11679c361e0dcc5aa5bcb
Rafaela Inácio <rafaelafsinacio@gmail.com>
2023-05-20 13:43:27 -0300

    config.js indentation

--- db/config.js
+++ db/config.js
@@ -1,3 +1,3 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/todo-list')
+mongoose.connect("mongodb://localhost:27017/todo-list");
