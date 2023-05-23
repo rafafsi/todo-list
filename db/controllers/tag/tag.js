a7ac86d3429fa599e76bb03f2a8888d8dbb373a0
Rafaela Inácio <rafaelafsinacio@gmail.com>
2023-05-23 10:43:58 -0300

    adding searchTasks function to controller file and to route

--- db/controllers/tag/tag.js
+++ db/controllers/tag/tag.js
@@ -5,7 +5,7 @@ module.exports = {
   create: async (req, res) => {
     try {
       const { name, color } = await req.body;
-      const tag = await Tag.create({ name, color }); 
+      const tag = await Tag.create({ name, color });
       await tag.save();
       let taskById = await Task.findByIdAndUpdate(req.params.id, {
         $push: {
@@ -46,6 +46,41 @@ module.exports = {
       await res.send(deletedTag);
     } catch (error) {
       res.send(error.message);
+    }
+  },
+  searchTasks: async (req, res) => {
+    const urlParam = req.query.tag;
+
+    const findTagId = async (tagName) => {
+      return await Tag.findOne({ name: tagName }).then((tag) => tag._id);
+    };
+
+    const populateTask = async (tagId) => {
+      return await Task.find(
+        { tags: tagId },
+        "-_id -description -status -__v -priority"
+      ).populate("tags", "-_id -description -status -__v");
+    };
+
+    if (urlParam.includes(",")) {
+      try {
+        const params = urlParam.split(",");
+
+        const ids = await Promise.all(
+          params.map((tag) => {
+            return findTagId(tag);
+          })
+        );
+
+        const records = await Task.find({ tags: { $all: ids } });
+
+        res.send(records);
+      } catch (error) {
+        res.send(error.message);
+      }
+    } else {
+      const result = await findTagId(urlParam).then((id) => populateTask(id));
+      res.send(result);
     }
   },
 };
--- db/routes.js
+++ db/routes.js
@@ -11,12 +11,13 @@ router.get("/api/task/find/:id", Task.find);
 router.delete("/api/task/delete/:id", Task.delete);
 router.get("/api/task/populate/:id", Task.populateTags);
 router.post("/api/task/add-tag/:taskId/:tagName", Task.addTagToATask);
+router.delete("/api/task/remove-tag/:taskId/:tagName", Task.removeTagFromATask);
 
 //tag routes
 router.post("/api/tag/create/:id", Tag.create);
 router.get("/api/tag/find/:id", Tag.find);
 router.post("/api/tag/update/:id", Tag.update);
 router.delete("/api/tag/delete/:id", Tag.delete);
-// router.get("/api/tag/search-tasks", Tag.searchTasks);
+router.get("/api/tag/search-tasks", Tag.searchTasks);
 
 module.exports = router;
